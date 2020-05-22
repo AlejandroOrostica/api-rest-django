0b709b1e894c1f79d0e90099e5014056cfa293a7
Alejandro <alejandro.orostica@usach.cl>
2020-05-22 03:40:42 -0400

    Ready for deploy

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p1j7dxr+*gi@^-sl^ok-1oz6i9n6ye*j9wq*!k&plpoyl+q9b3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
